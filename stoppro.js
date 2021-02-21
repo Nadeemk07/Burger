66cd0ba44006de7312669e2547057e68be029710
Nadeemk07 <eznizer@gmail.com>
2021-02-21 14:40:09 +0530

    add

---
+++ stoppro.js
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Nadeem Kaisar javascript</title>
+</head>
+<body>
+<div id="first"> 
+     <div id="second"> 
+      <button>Click here</button>
+
+     </div>
+</div>	
+
+<script type="text/javascript">
+let a=document.querySelector("#first")
+let b=document.querySelector("#second")
+a.addEventListener("mousedown",event=>{
+	conlog.log("Outer div")
+});
+
+b.addEventListener("mousedown",event=>{
+	conlog.log("inner div")
+});
+button.addEventListener("mousedown",event=>{
+	if(event.button==2) event.stopPropagation();
+});
+
+
+</script>
+</body>
+</html>
